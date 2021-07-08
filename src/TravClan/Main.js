46ee0fe71b448d0e54c03c2dec1575043ea2dcfd
NaveenKumar-1209 <nkc.2096@gmail.com>
2021-07-09 00:23:10 +0530

    init

--- src/TravClan/Main.js
+++ src/TravClan/Main.js
@@ -1,11 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from "axios";
-
+import Header from './Header';
 const Main = () => {
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [postsPerPage, setPostsPerpage] = useState(10);
 
-    return (
-        <div>
-            <h1>Hello World</h1>
+    useEffect(()=>{
+        const fetchPosts = async () =>{
+            setLoading(true);
+            const res = await axios.get("https://intense-tor-76305.herokuapp.com/merchants");
+            setPosts(res.data);
+            setLoading(false);
+        }
+        fetchPosts();
+    },[]);
+        return (
+        <div className = "Container mt-5">
+            <h1 className="text-primary mb-3">TravClan</h1>
+            <Header posts={posts} loading={loading} />
         </div>
     )
 }
