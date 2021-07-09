602427028acfe37041f657d2306a2f5d7d95f384
NaveenKumar-1209 <nkc.2096@gmail.com>
2021-07-09 10:34:47 +0530

    seventh change

--- src/TravClan/Main.js
+++ src/TravClan/Main.js
@@ -3,11 +3,14 @@ import axios from "axios";
 import Customers from './Customers';
 import Pagination from './Pagination';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "./Main.css";
+import { Navbar } from 'react-bootstrap';
+
 const Main = () => {
     const [posts, setPosts] = useState([]);
     const [loading, setLoading] = useState(false);
     const [currentPage, setCurrentPage] = useState(1);
-    const [postsPerPage, setPostsPerpage] = useState(1);
+    const [postsPerPage, setPostsPerPage] = useState(1);
 
     useEffect(()=>{
         const fetchPosts = async () =>{
@@ -23,12 +26,16 @@ const Main = () => {
     const indexOfFirstPost = indexOfLastPost - postsPerPage;
     const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
         return (
-        <div className = "Container mt-5">
-            <h1 className="text-primary mb-3">TravClan</h1>
+        <div>
+            <Navbar style={{backgroundColor:"#101370"}} expand="lg">
+            <Navbar.Brand style={{marginLeft:"65px"}} href="#">
+            <img src="https://images.assets-landingi.com/j0Cv6OsfQEt2qHeH/travclan_logo_latest.png" />
+            </Navbar.Brand>
+            </Navbar>
             <Customers posts={currentPosts} loading={loading} />
             <Pagination postsPerPage={postsPerPage} totalPosts={posts.length} />
         </div>
     )
 }
 
-export default Main
+export default Main;
